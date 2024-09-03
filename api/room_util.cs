737795380726227d491ced363eb120a541e7cefd
wiiboi69 <meereaareea@gmail.com>
2024-09-03 16:25:54 +0100

    Update room_util.cs

--- api/room_util.cs
+++ api/room_util.cs
@@ -39,8 +39,9 @@ namespace api
                     Console.WriteLine("found room name: " + temp2 + " using room id: " + temp1);
                     return new WebClient().DownloadString("https://raw.githubusercontent.com/wiiboi69/Rec_rewild_server_data/main/rooms_name/" + temp2.ToLower() + ".txt").ToString();
                 }
-                catch
+                catch (Exception e)
                 {
+                    Console.WriteLine(e.Message);
                     goto roomfaileddownload;
                 }
             }
