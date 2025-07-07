5c282a0249cee04e1e37773fb3a6869ab98a6342
e <120246447+nito9999@users.noreply.github.com>
2025-07-07 10:33:55 -0400

    Update apiserver.cs

--- servers/apiserver.cs
+++ servers/apiserver.cs
@@ -586,16 +586,7 @@ namespace server
                         }
                         if (Url.StartsWith("storefronts/v4/balance/2"))
                         {
-                            var balance = new[]
-                            {
-                             new
-                             {
-                                 Balance = Convert.ToUInt64(File.ReadAllText("SaveData\\Profile\\tokens.txt")),
-                                 BalanceType = -2,
-                                 CurrencyType = 2
-                             }
-                           };
-                            s = JsonConvert.SerializeObject(balance);
+                           
                         }
 
                         if (Url == "gamerewards/v1/pending")
